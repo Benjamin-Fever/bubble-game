4b07173a7e446e31ded85a56305cb2dac38f67da
Benjamin-Fever <ben.fever07@gmail.com>
2025-01-25 23:45:31 +1300

    remove path draw

--- entitys/enemy/ChasePlayerState.cs
+++ entitys/enemy/ChasePlayerState.cs
@@ -6,18 +6,6 @@ public partial class ChasePlayerState : State {
     [Export] private float ChaseSpeed = 100;
 	[Export] private SpeedComponent speedComponent;
 
-    public override void _EnterTree() {
-        base._EnterTree();
-		speedComponent.Body.Draw += () => {
-			Vector2 targetPos = GetTree().Root.GetNode<CharacterBody2D>("Main/Player").GlobalPosition;
-        	Array<Vector2> path = SceneManager.GetPathToPoint(speedComponent.Body.GlobalPosition, targetPos);
-			if (path.Count == 0) return;
-			for (int i = 0; i < path.Count - 1; i++) {
-				speedComponent.Body.DrawLine(speedComponent.Body.ToLocal(path[i]), speedComponent.Body.ToLocal(path[i + 1]), new Color(1, 0, 0), 2);
-			}
-		};
-    }
-
     public override void Update(double delta) {
         Vector2 targetPos = GetTree().Root.GetNode<CharacterBody2D>("Main/Player").GlobalPosition;
         Array<Vector2> path = SceneManager.GetPathToPoint(speedComponent.Body.GlobalPosition, targetPos);
@@ -34,7 +22,6 @@ public partial class ChasePlayerState : State {
 			return;
 		}
 		speedComponent.Velocity = direction.Normalized() * ChaseSpeed * (float)delta;
-		speedComponent.Body.QueueRedraw();
     }
 
     public override bool CanChangeState() {
